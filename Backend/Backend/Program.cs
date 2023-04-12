c86bba46663429142653b06abd3766e85e9cb1db
Luis Fernando Rojas González <luisfernando.lzc@hotmail.com>
2023-04-12 01:00:17 -0600

    Connected Backend with DB

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,10 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using Backend;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
+var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+        });
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -19,6 +32,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
