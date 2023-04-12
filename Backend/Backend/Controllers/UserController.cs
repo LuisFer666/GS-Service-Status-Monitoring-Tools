a1ee54f56c4a8eaa873ef234b2b9889e19ed1e2d
Luis Fernando Rojas González <luisfernando.lzc@hotmail.com>
2023-04-12 08:30:32 -0600

    User autentication

---
+++ Backend/Backend/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public UserController(ILogger<UserController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Auth")]
+        public ActionResult Auth([FromBody] User user)
+        {
+            var connectionString = _configuration.GetConnectionString("MyDB");
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("[dbo].[AuthUser]", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("@Username", SqlDbType.VarChar).Value = user.Username;
+                    command.Parameters.Add("@Password", SqlDbType.VarChar).Value = user.Password;
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if(reader.Read())
+                        {
+                            if(reader.GetInt32(0) == 1)
+                            {
+                                return Ok(1);
+                            }
+                            else
+                            {
+                                return Unauthorized(0);
+                            }
+                        }else
+                        {
+                            return BadRequest();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
--- DB/CreateProcedures/AuthUser.sql
+++ DB/CreateProcedures/AuthUser.sql
@@ -2,11 +2,13 @@ USE [Az-Ejemplo1]
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[AuthUser]
-	@User varchar(200),
+	@Username varchar(200),
 	@Password varchar(200)
 AS
 BEGIN
-	IF EXISTS (SELECT * FROM [dbo].[User] as u WHERE u.Username = @User and u.Password = @Password)
+	SET NOCOUNT ON;
+
+	IF EXISTS (SELECT * FROM [dbo].[User] as u WHERE u.Username = @Username and u.Password = @Password)
 	BEGIN
 		SELECT 1;
 	END
